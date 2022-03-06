3edb8ec26789ce9d7ec0379af9f21cf83b9df201
th7024 <th7024@gmail.com>
2022-03-06 20:54:28 +0900

    PGM 124 나라의 숫자

---
+++ PGM/124 나라의 숫자/Q220306_1.java
@@ -0,0 +1,23 @@
+package Programmers;
+//124 나라의 숫자
+public class Q220306_1 {
+	class Solution {
+		  public String solution(int n) {
+		      String[] numbers = {"4", "1", "2"};
+		      String answer = "";
+		      
+		      int num = n;
+		      
+		      while(num > 0){
+		          int remainder = num % 3;
+		          num /= 3;
+		          
+		          if(remainder == 0) num--;
+		          
+		          answer = numbers[remainder] + answer;
+		      }
+		      
+		      return answer;
+		  }
+		}
+}
